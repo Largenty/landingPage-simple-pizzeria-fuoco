9bcc77461030debea20b8237db769a3a366ecdb3
Largenty <Largenty@gmail.com>
2025-10-07 16:43:33 +0200

    lint modification

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,35 +3,35 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Pizzeria Fuoco",
-  description: "The best pizza in town",
-  icons: {
-    icon: "/pizzria-fuoco.png",
-  },
+    title: "Pizzeria Fuoco",
+    description: "The best pizza in town",
+    icons: {
+        icon: "/pizzria-fuoco.png",
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
 }
